e2473fcb63628f33ca592bb591ff3c38d9465375
Fabuland <45296816+Fabuland@users.noreply.github.com>
2020-02-18 20:51:51 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,37 @@
 $("[data-toggle=popover]").popover();
-$('.carousel').carousel({
-    interval: 2000
-})
-$("#btnShow").click(function(){
-  
-    $(".alert").hide().show('medium');
-  });
+$(".carousel").carousel({
+  interval: 2000
+});
+$("#btnShow").click(function() {
+  $(".alert")
+    .hide()
+    .show("medium");
+});
+
+$("#btnLogin").click(function() {
+  $("#myModal").modal();
+});
+
+(function() {
+  "use strict";
+  window.addEventListener(
+    "load",
+    function() {
+      var forms = document.getElementsByClassName("needs-validation");
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener(
+          "submit",
+          function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
